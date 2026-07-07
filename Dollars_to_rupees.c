4458e98226fce9880d7db673dfa9b8d483f01bca
BhagyaSree-2007 <bsree257_bcs29@mepcoeng.ac.in>
2026-07-07 13:12:41 +0530

    Create Dollars_to_rupees.c
    Write a C program to convert dollar into rupees

---
+++ Dollars_to_rupees.c
@@ -0,0 +1,11 @@
+/*Write a C program to convert dollar into rupees*/
+#include<stdio.h>
+int main()
+{
+        float d,r;
+        printf("Enter amount in Dollars:");
+        scanf("%f",&d);
+        r=88.3*d;
+        printf("In rupees:%f\n",r);
+        return 0;
+}
